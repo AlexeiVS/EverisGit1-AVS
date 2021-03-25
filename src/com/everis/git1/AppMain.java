187ac1012e7d41fa0612716d219d0152c7192861
AlexeiVS <aggressive_fenix666@hotmail.com>
2021-03-25 10:54:54 +0100

    Desarrollo V 1.0

---
+++ src/com/everis/git1/AppMain.java
@@ -0,0 +1,21 @@
+package com.everis.git1;
+
+/**
+ * Desafio GIT
+ * 
+ * @author Alexei Viadero
+ *
+ */
+public class AppMain {
+	/**
+	 * Metodo principal
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
